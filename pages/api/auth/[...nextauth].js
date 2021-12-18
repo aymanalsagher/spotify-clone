444c450e4e774f9ef2103d35189d845e0a83c5cc
aymanalsagher <aymanalsagher@gmail.com>
2021-12-18 13:47:54 +0200

    reverting back

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -35,8 +35,7 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-  // secret: process.env.JWT_SECRET,
-  secret: process.env.SECRET,
+  secret: process.env.JWT_SECRET,
   pages: {
     signIn: "/login",
   },
